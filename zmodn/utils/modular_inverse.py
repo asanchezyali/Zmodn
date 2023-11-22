a80973fae90f2e032671d2bb800338688b3d116a
Alejandro Sánchez Yalí <asanchezyali@gmail.com>
2023-11-22 09:29:43 -0500

    updata vectorize modular inverse function

--- zmodn/utils/modular_inverse.py
+++ zmodn/utils/modular_inverse.py
@@ -1,28 +1,15 @@
-import math
 import numpy as np
 
 
-def modular_inverse(integer, module):
-    if not isinstance(integer, (np.int64, int)):
-        raise TypeError("Integer must be an integer")
+def vectorize_modular_inverse(integers, module):
+    if not isinstance(integers, np.ndarray):
+        raise TypeError("Integers must be a numpy array")
+    if not np.issubdtype(integers.dtype, np.integer):
+        raise TypeError("Integers must be an array of integers")
     if not isinstance(module, (np.int64, int)):
         raise TypeError("Module must be an integer")
     if not module > 0:
         raise ValueError("Module must be positive")
-    if not math.gcd(integer, module) == 1:
-        raise ValueError("Integer and module must be coprime")
-
-    # Extended Euclidean algorithm
-    # https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm
-    aux1 = 0
-    aux2 = 1
-    y = integer
-    x = module
-    while y != 0:
-        q, r = divmod(x, y)
-        x, y = y, r
-        aux1, aux2 = aux2, aux1 - q * aux2
-    return aux1 % module
-
-
-vectorize_modular_inverse = np.vectorize(modular_inverse)
+    if not np.all(np.gcd(integers, module) == 1):
+        raise ValueError("All integers and module must be coprime")
+    return np.vectorize(lambda integer, module: pow(integer, module - 2, module), otypes=[int])(integers, module)
